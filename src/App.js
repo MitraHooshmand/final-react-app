2d24409810b95ce08becc8176941b027570b554b
mita4824 <87270333+mita4824@users.noreply.github.com>
2021-12-04 18:37:14 +0330

    add git repo URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
       hello
+      <p>
+        coded by mitra{" "}
+        <a href="https://github.com/mita4824/final-react-app" target="_blank">
+          open source
+        </a>
+      </p>
     </div>
   );
 }
