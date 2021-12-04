d10fe620e3c0f455cc5da999900c1c4a04d2de5b
mita4824 <87270333+mita4824@users.noreply.github.com>
2021-12-04 22:27:27 +0330

    adding CSS

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ function App() {
     <div className="App">
       <div className="container">
         <Weather />
-
-        <p>
+        <footer>
           This Project was coded by Mitra and is{" "}
           <a href="https://github.com/mita4824/final-react-app">
             open source on Github
           </a>
-        </p>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a City..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primery" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,19 @@ export default function Weather() {
         <li>Saturday 11:00</li>
         <li>Mostly sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly sunny"
-          ></img>
-          6°
+          <div className="clearFix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly sunny"
+              className="float-left "
+            ></img>
+            {/* <div className="float-left"> */}
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+            {/* </div> */}
+          </div>
         </div>
         <div className="col-6">
           <ul>
